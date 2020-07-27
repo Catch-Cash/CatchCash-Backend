403e7d8136fa2ecdb1396304f79263276743bf8f
hagyeong <gkrud4513@naver.com>
2020-07-27 14:54:44 +0900

    [UPDATE] update app.js jwt

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+const createError = require('http-errors');
+
+const config = require('./config/config');
+const sequelize = require('./models/index').sequelize;
+const api = require('./api');
+
+const app = express();
+sequelize.sync();
+
+const port = process.env.PORT;
+
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/',api);
+app.set('jwt-secret', config.secret);
+
+app.listen(port, () => {
+    console.log(`Express server is running at ${port}`);
+});
