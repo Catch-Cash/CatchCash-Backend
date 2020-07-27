4e91d9dc55fa4e2228d035ec7ee64aa6d2b29115
hagyeong <gkrud4513@naver.com>
2020-07-27 14:53:34 +0900

    [UPDATE] update config

---
+++ config/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'secret': process.env.secret,
+  'APPID': 'c78606f593c42e75a58d7472d0264082',
+  client_id: 'yeG3GRs3ctr5KMgMCeOqppTYfh72yJJ2UrnjguIR',
+  client_secret:'H93CF6YGzC8Cuu4haZ5SQM9r6lLXiGPhNdEYffNf',
+
+}
