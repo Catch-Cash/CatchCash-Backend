73d8ae3690b51d09e25cbc6e7665bca463f5485c
so_ohi <jsh167528@gmail.com>
2020-07-29 11:36:29 +0900

    Slove merge conflict

--- api/auth/auth.controller.js
+++ api/auth/auth.controller.js
@@ -1,140 +1,188 @@
-const config = require('../../config/config');
-const {User} = require('../../models');
-const jwt = require('jsonwebtoken');
-const request = require('request');
-const createError = require('http-errors');
+const config = require("../../config/config");
+const { User } = require("../../models");
+const jwt = require("jsonwebtoken");
+const request = require("request");
+const createError = require("http-errors");
 
-const authorize = (req,res)=>{
-  try{
+const authorize = (req, res) => {
+  try {
+    // const option = {
+    //   uri: "https://testapi.openbanking.or.kr/oauth/2.0/authorize",
+    //   header: "",
+    //   qs: {
+    //     response_type: "code",
+    //     client_id: config.client_id,
+    //     redirect_uri: "http://localhost:1212/authorize/callback",
+    //     scope: "login inquiry transfer",
+    //     state: "12345678901234567890122345689012"
+    //   }
+    // };
+
     const option = {
       uri: "https://testapi.openbanking.or.kr/oauth/2.0/authorize",
-      header:'',
-      qs:{
-        response_type:"code",
+      header: "",
+      qs: {
+        response_type: "code",
         client_id: config.client_id,
         redirect_uri: "http://localhost:1212/authorize/callback",
         scope: "login inquiry transfer",
         state: "12345678901234567890122345689012"
       }
     };
-  
-    request.get(option,(err,response,body)=>{
-      res.json({url:response.request.url.href});
-    });
-  }catch(e){
-    res.status(400).json(e).end();
-  }
-}
 
-const authorize_callback = async (req,res)=>{
-  try{
+    // const option = {
+    //   uri: "https://testapi.openbanking.or.kr/v2.0/account/list",
+    //   headers: {
+    //     Authorization: `Bearer ${user.dataValues.access_token}`
+    //   },
+    //   qs: {
+    //     user_seq_no: user.dataValues.user_seq_no,
+    //     sort_order: "D",
+    //     include_cancel_yn: "Y"
+    //   }
+    // };
+
+    request.get(option, (err, response, body) => {
+      res.json({ url: response.request.url.href });
+    });
+  } catch (e) {
+    res
+      .status(400)
+      .json(e)
+      .end();
+  }
+};
+
+const authorize_callback = async (req, res) => {
+  try {
     const option = {
       uri: "https://testapi.openbanking.or.kr/oauth/2.0/token",
-      header:'',
-      form:{
-        code:req.query.code,
+      header: "",
+      form: {
+        code: req.query.code,
         client_id: config.client_id,
-        client_secret:config.client_secret,
+        client_secret: config.client_secret,
         redirect_uri: "http://localhost:1212/authorize/callback",
-        grant_type: 'authorization_code'  
+        grant_type: "authorization_code"
       }
     };
-  
-    request.post(option,async (err,response,body)=>{
+
+    request.post(option, async (err, response, body) => {
       const accessTokenRequestResult = JSON.parse(body);
-      const user = User.findOne({where:{user_seq_no:accessTokenRequestResult.user_seq_no}});
-      if(!user.dataValues){
-        User.create({
-          user_seq_no:accessTokenRequestResult.user_seq_no,
-          access_token:accessTokenRequestResult.access_token,
-          refresh_token:accessTokenRequestResult.refresh_token
-        }).catch(e=>{
-          throw new Error(e.message);
-        })
-      }else{
-        User.update({
-          access_token:accessTokenRequestResult.access_token,
-          refresh_token:accessTokenRequestResult.refresh_token
-        },{
-          where:{user_seq_no:accessTokenRequestResult.user_seq_no}
-        });
-      }
-      const secret = req.app.get('jwt-secret');
-      const token = await jwt.sign({user_seq_no:accessTokenRequestResult.user_seq_no}, secret, {
-        expiresIn: '100h',
-        issuer: 'gkrud',
-        subject: 'user_info'
+      const user = User.findOne({
+        where: { user_seq_no: accessTokenRequestResult.user_seq_no }
       });
-  
+
+      if (!user.dataValues) {
+        User.create({
+          user_seq_no: accessTokenRequestResult.user_seq_no,
+          access_token: accessTokenRequestResult.access_token,
+          refresh_token: accessTokenRequestResult.refresh_token
+        }).catch(e => {
+          throw new Error(e.message);
+        });
+      } else {
+        console.log(123);
+        User.update(
+          {
+            access_token: accessTokenRequestResult.access_token,
+            refresh_token: accessTokenRequestResult.refresh_token
+          },
+          {
+            where: { user_seq_no: accessTokenRequestResult.user_seq_no }
+          }
+        );
+      }
+      const secret = req.app.get("jwt-secret");
+
+      const token = await jwt.sign(
+        { user_seq_no: accessTokenRequestResult.user_seq_no },
+        secret,
+        {
+          expiresIn: "100h",
+          issuer: "gkrud",
+          subject: "user_info"
+        }
+      );
+
       res.json({
-        access_token:token,
-        refresh_token:accessTokenRequestResult.refresh_token
+        access_token: token,
+        refresh_token: accessTokenRequestResult.refresh_token
       });
     });
-  }catch(e){
-    res.status(400).json(e.message).end();
+  } catch (e) {
+    res
+      .status(400)
+      .json(e.message)
+      .end();
   }
-}
+};
 
-const logout = async (req,res)=>{
-  try{
+const logout = async (req, res) => {
+  try {
     const user_seq_no = req.decoded.user_seq_no;
     const user = await User.findOne({
-      where:{user_seq_no:user_seq_no}
+      where: { user_seq_no: user_seq_no }
     });
-  
+
     const option = {
       uri: "https://testapi.openbanking.or.kr/v2.0/user/unlink",
-      headers:{
+      headers: {
         Authorization: `Bearer ${user.dataValues.access_token}`
       },
-      form:{
-        client_use_code: 'T991634670',
-        user_seq_no: user.dataValues.user_seq_no  
+      form: {
+        client_use_code: "T991634670",
+        user_seq_no: user.dataValues.user_seq_no
       }
     };
-  
-    request.post(option,(err,response,body)=>{
-      const res_body = JSON.parse(body);
-      if(!res_body.api_tran_id) res.json({message:"failed logout"})
-      else res.json({message: "success logout"});
-    });
-  }catch(e){
-    res.status(400).json(e.message).end();
-  }
-}
 
-const secession = async (req,res)=>{
-  try{
+    request.post(option, (err, response, body) => {
+      const res_body = JSON.parse(body);
+      if (!res_body.api_tran_id) res.json({ message: "failed logout" });
+      else res.json({ message: "success logout" });
+    });
+  } catch (e) {
+    res
+      .status(400)
+      .json(e.message)
+      .end();
+  }
+};
+
+const secession = async (req, res) => {
+  try {
     const user_seq_no = req.decoded.user_seq_no;
     const user = await User.findOne({
-      where:{user_seq_no:user_seq_no}
+      where: { user_seq_no: user_seq_no }
     });
-    if(!user) throw new Error('no exist user');
+    if (!user) throw new Error("no exist user");
     const option = {
       uri: "https://testapi.openbanking.or.kr/v2.0/user/close",
-      headers:{
+      headers: {
         Authorization: `Bearer ${user.dataValues.access_token}`
       },
-      form:{
-        client_use_code: 'T991634670',
-        user_seq_no: user.dataValues.user_seq_no  
+      form: {
+        client_use_code: "T991634670",
+        user_seq_no: user.dataValues.user_seq_no
       }
     };
-    request.post(option,async(err,response,body)=>{
-      await User.destroy({where:{user_seq_no:user_seq_no}});
+    request.post(option, async (err, response, body) => {
+      await User.destroy({ where: { user_seq_no: user_seq_no } });
       const res_body = JSON.parse(body);
-      if(!res_body.api_tran_id) res.json({message:"failed logout"})
-      else res.json({message: "success secession"});
+      if (!res_body.api_tran_id) res.json({ message: "failed logout" });
+      else res.json({ message: "success secession" });
     });
-  }catch(e){
-    res.status(400).json(e.message).end();
+  } catch (e) {
+    res
+      .status(400)
+      .json(e.message)
+      .end();
   }
-}
+};
 
 module.exports = {
   authorize,
   authorize_callback,
   logout,
   secession
-}
+};
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const morgan = require('morgan');
-const createError = require('http-errors');
+const express = require("express");
+const morgan = require("morgan");
+const createError = require("http-errors");
 
-const config = require('./config/config');
-const sequelize = require('./models/index').sequelize;
-const api = require('./api');
+const config = require("./config/config");
+const sequelize = require("./models/index").sequelize;
+const api = require("./api");
 
 const app = express();
 sequelize.sync();
@@ -12,16 +12,10 @@ sequelize.sync();
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(morgan('dev'));
-app.use('/',api);
-app.set('jwt-secret', config.secret);
+app.use(morgan("dev"));
+app.use("/", api);
+app.set("jwt-secret", config.secret);
 
 app.listen(port, () => {
-    console.log(`Express server is running at ${port}`);
-});
-
-process.once("SIGUSR2", function() {
-  server.close(function() {
-    process.kill(process.pid, "SIGUSR2");
-  });
+  console.log(`Express server is running at ${port}`);
 });
