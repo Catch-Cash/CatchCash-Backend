0071d65a0a7ee70649b774acddc843d4e5bbce6a
so_ohi <jsh167528@gmail.com>
2020-07-30 09:15:43 +0900

    Change config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 module.exports = {
-  'secret': process.env.secret,
-  'APPID': 'c78606f593c42e75a58d7472d0264082',
-  client_id: 'yeG3GRs3ctr5KMgMCeOqppTYfh72yJJ2UrnjguIR',
-  client_secret:'H93CF6YGzC8Cuu4haZ5SQM9r6lLXiGPhNdEYffNf',
-
-}
+  secret: "SeCrEtKeYfOrHaShInG",
+  APPID: "c78606f593c42e75a58d7472d0264082",
+  client_id: "yeG3GRs3ctr5KMgMCeOqppTYfh72yJJ2UrnjguIR",
+  client_secret: "H93CF6YGzC8Cuu4haZ5SQM9r6lLXiGPhNdEYffNf"
+};
